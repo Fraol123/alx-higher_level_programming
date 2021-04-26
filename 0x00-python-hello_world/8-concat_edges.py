04bb2a481bddd8596f779ee37501ea4b8d70b050
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-04-26 14:07:57 +0000

    cuted an concated

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = " Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power  with very clear syntax"
+str = str[40:67] + str[-23:-18] + str[0:7]
+print(str)
