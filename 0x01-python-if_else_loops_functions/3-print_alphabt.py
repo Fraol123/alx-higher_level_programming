7e1b70a38f79674a634494e48a8df7f1dd6ee2b2
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-04-27 10:42:29 +0000

    small l

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for lower in range(97,123):
-    if chr(lower) is not 'q' and chr(lower) is not 'e':
-        print("{}".format(chr(lower)),end="")
+for sletter in range(97,123):
+    if chr(sletter) is not 'q' and chr(sletter) is not 'e':
+        print("{}".format(chr(sletter)),end="")
