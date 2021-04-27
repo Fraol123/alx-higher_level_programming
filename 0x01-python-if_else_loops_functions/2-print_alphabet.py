d1001b8020358a138fc2f1b5bd861543861e88d5
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-04-27 09:14:26 +0000

    lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-"""print alphabet in lowercase"""
-for i in range(97,123):
-    print("{}".format(chr(i)),end="")
+for lower in range(97,123):
+    print("{}".format(chr(lower)),end="")
