26f78b0ca4af9df04452e2f22049e07c2a21f3a6
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-04-29 08:28:22 +0000

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+length = (len(sys.argv) - 1)
+
+if length == 1:
+    print("1 argument:")
+elif length == 0:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(length))
+
+for x in range(1, len(sys.argv)):
+    print("{:d}:".format(x), str(sys.argv[x]))
