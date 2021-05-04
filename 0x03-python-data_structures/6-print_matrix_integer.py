77b262bee612c6c31f8c76c75c685cc25040801c
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-04 12:38:47 +0000

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][column]), end=" ")
+            if column != len(matrix[row]) - 1:
+                print("{}".format(""), end=" ")
+        print()
