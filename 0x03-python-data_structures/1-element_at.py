135c4b7aa5d2dbc1192fa2ce2b866632a98420dc
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-04 08:38:33 +0000

    elements

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that retrieves an element from a list like in C
+"""
+
+
+def element_at(my_list, idx):
+        if 0 <= idx < idx < (len(my_list)):
+                return my_list[idx]
+        else:
+                return None
