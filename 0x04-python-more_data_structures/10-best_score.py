5bdf45e8fdb23731d5297ffed3a2e412b6f27037
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-06 13:31:34 +0000

    best sco

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,10 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    if a_dictionary:
-        return max(a_dictionary)
+    com = list(a_dictionary.keys())[0]
+    large = a_dictionary[com]
+    for k, v in a_dictionary.items():
+        if v > large:
+            large = v
+            com = k
+    return com
