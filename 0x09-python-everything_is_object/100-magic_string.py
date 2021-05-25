d75910e9abef8263008f7c993b922f216c9dd2fc
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-25 14:28:42 +0000

    go

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(mag=[]):
+    mag += ['Holberton']
+    return ', '.join(mag)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""blocked class module"""
+
+
+class LockedClass:
+    """object prevents dynamic attribute"""
+
+    __slots__ = ['first_name']
+    
