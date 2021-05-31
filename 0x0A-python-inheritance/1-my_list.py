3a4fea54524bdfed6f47fb750b36d09e15b08f21
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-31 11:47:29 +0000

    handled with test file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for sorting"""
+
+
+
+class MyList(list):
+    """sub class of list"""
+
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
