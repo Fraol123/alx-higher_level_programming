098896a95e66e09b8d15f2f579bc6bdb9464460b
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-31 12:30:57 +0000

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geomtrt module"""
+
+
+class BaseGeometry:
+    """empty geomtry module"""
+    def area(self):
+        """raises error"""
+        raise Exception("area() is not implemented")
