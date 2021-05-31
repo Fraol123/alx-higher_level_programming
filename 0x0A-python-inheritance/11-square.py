e964045fe1fcfb1758ce834724164efab19b4fdc
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-31 17:33:37 +0000

    squared

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -14,3 +14,12 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+        def area(self):
+            '''Calculates the area of a square'''
+            return self.__size * self.__size
+
+        def __str__(self):
+            '''Return the square representation'''
+            msg = '[Square] {}/{}'.format(self.__size, self.__size)
+            return msg
