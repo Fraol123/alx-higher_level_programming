a0f88603c4b4768b0d9133b3ec0e387eafd8b8a6
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-06-01 13:54:26 +0000

    edited for new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a text file"""
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
