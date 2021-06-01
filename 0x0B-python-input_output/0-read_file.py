9e8f5f436d056d53b0c857e11bc63bb195bb3350
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-06-01 09:11:29 +0000

    read from file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Reading module"""
+
+
+def read_file(filename=""):
+    """reads a text file"""
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read())
