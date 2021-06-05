858063aca479f380e05fa0ab1a879518e6e7ab5b
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-06-05 15:47:06 +0000

    until task1

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,63 @@
 #!/user/bin/python3
+import unittest
+import json
+from models.rectangle  import Rectangle
+from models.base import Base
+
+class TestRectangle_instantiation(unittest.TestCase):
+    """unitest for checking rectangle instantiation"""
+
+    def test_rectangle_is_base(self):
+        self.assertIsInstance(Rectangle(10, 2), Base)
+
+    def test_no_args(self):
+        with self.assertRaises(TypeError):
+           Rectangle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ == "__main__":
+        unittest.main()
