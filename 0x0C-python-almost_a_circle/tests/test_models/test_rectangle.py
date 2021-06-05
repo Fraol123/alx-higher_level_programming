21b71455c5131720e27321e6f296311917e10a05
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-06-05 16:48:23 +0000

    testing private

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -29,9 +29,33 @@ class TestRectangle_instantiation(unittest.TestCase):
         self.assertEqual(r1.id, r2.id - 1)
 
 
+    def test_four_args(self):
+        r1 = Rectangle(3,5,6,4)
+        r2 = Rectangle(4,6,5,3)
+        self.assertEqual(r1.id, r2.id - 1)
 
+    def test_five_args(self):
+        self.assertEqual(3, Rectangle(10, 2, 0, 0, 3).id)
 
+    def test_more_than_five_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, 4, 5, 6, 7)
+        
+    def test_width_private(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(2,3,4,2,1).__widtth)
 
+    def test_height_private(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(2,3,4,2,1).__height)
+
+    def test_x_private(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(2,3,4,2,1).__x)
+
+    def test_y_private(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(2,3,4,2,1).__y)
 
 
 
