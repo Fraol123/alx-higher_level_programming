3b636887e2102a313931ffc18b57ff0027f384fd
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-06-08 09:04:19 +0000

    some tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -224,5 +224,120 @@ class TestSquare_y(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "y must be >= 0"):
             Square(3, 0, -1)
 
+
+class TestSquare_order_of_initialization(unittest.TestCase):
+    """Unittests for testing order of Square attribute initialization."""
+
+    def test_size_before_x(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("invalid size", "invalid x")
+
+    def test_size_before_y(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("invalid size", 1, "invalid y")
+
+    def test_x_before_y(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, "invalid x", "invalid y")
+
+
+class TestSquare_area(unittest.TestCase):
+    """Unittests for testing the area method of the Square class."""
+
+    def test_area_small(self):
+        self.assertEqual(100, Square(10, 0, 0, 1).area())
+
+    def test_area_large(self):
+        s = Square(999999999999999999, 0, 0, 1)
+        self.assertEqual(999999999999999998000000000000000001, s.area())
+
+    def test_area_changed_attributes(self):
+        s = Square(2, 0, 0, 1)
+        s.size = 7
+        self.assertEqual(49, s.area())
+
+    def test_area_one_arg(self):
+        s = Square(2, 10, 1, 1)
+        with self.assertRaises(TypeError):
+            s.area(1)
+
+
+class TestSquare_stdout(unittest.TestCase):
+    """Unittests for testing __str__ and display methods of Square class."""
+
+    @staticmethod
+    def hold_stdout(sq, method):
+        """Captures and returns text printed to stdout."""
+
+        hold = io.StringIO()
+        sys.stdout = hold
+        if method == "print":
+            print(sq)
+        else:
+            sq.display()
+        sys.stdout = sys.__stdout__
+        return hold
+
+    def test_str_method_print_size(self):
+        s = Square(4)
+        hold = TestSquare_stdout.hold_stdout(s, "print")
+        correct = "[Square] ({}) 0/0 - 4\n".format(s.id)
+        self.assertEqual(correct, hold.getvalue())
+
+    def test_str_method_size_x(self):
+        s = Square(5, 5)
+        correct = "[Square] ({}) 5/0 - 5".format(s.id)
+        self.assertEqual(correct, s.__str__())
+
+    def test_str_method_size_x_y(self):
+        s = Square(7, 4, 22)
+        correct = "[Square] ({}) 4/22 - 7".format(s.id)
+        self.assertEqual(correct, str(s))
+
+    def test_str_method_size_x_y_id(self):
+        s = Square(2, 88, 4, 19)
+        self.assertEqual("[Square] (19) 88/4 - 2", str(s))
+
+    def test_str_method_changed_attributes(self):
+        s = Square(7, 0, 0, [4])
+        s.size = 15
+        s.x = 8
+        s.y = 10
+        self.assertEqual("[Square] ([4]) 8/10 - 15", str(s))
+
+    def test_str_method_one_arg(self):
+        s = Square(1, 2, 3, 4)
+        with self.assertRaises(TypeError):
+            s.__str__(1)
+
+    # Test display method
+    def test_display_size(self):
+        s = Square(2, 0, 0, 9)
+        hold = TestSquare_stdout.hold_stdout(s, "display")
+        self.assertEqual("##\n##\n", hold.getvalue())
+
+    def test_display_size_x(self):
+        s = Square(3, 1, 0, 18)
+        hold = TestSquare_stdout.hold_stdout(s, "display")
+        self.assertEqual("###\n###\n###\n", hold.getvalue())
+
+    def test_display_size_y(self):
+        s = Square(4, 0, 1, 9)
+        hold = TestSquare_stdout.hold_stdout(s, "display")
+        display = "\n####\n####\n####\n####\n"
+        self.assertEqual(display, hold.getvalue())
+
+    def test_display_size_x_y(self):
+        s = Square(2, 3, 2, 1)
+        hold = TestSquare_stdout.hold_stdout(s, "display")
+        display = "\n\n##\n##\n"
+        self.assertEqual(display, hold.getvalue())
+
+    def test_display_one_arg(self):
+        s = Square(3, 4, 5, 2)
+        with self.assertRaises(TypeError):
+            s.display(1)
+
+
 if __name__ == "__main__":
     unittest.main()
