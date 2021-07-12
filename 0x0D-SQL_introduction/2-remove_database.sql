6eb38d88ea59145fe9ffefe33e4b579ca3a1c33d
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-12 17:17:47 +0000

    droped

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- a script that deletes the database hbtn_0c_0 in MySQL server
+-- If the database hbtn_0c_0 t exist, the script should not fail
+--  SELECT or SHOW statements are not allowed
+
+DROP DATABASE IF  EXISTS hbtn_0c_0;
