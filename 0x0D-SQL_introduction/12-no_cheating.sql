d7c8ef91538181e619434b61ee1ffad3ce9c9363
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-12 18:56:14 +0000

    bob set to 10

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- a script that updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use s id value, only the name field Bob
+
+UPDATE second_table SET score = 10 WHERE name = "Bob"; 
