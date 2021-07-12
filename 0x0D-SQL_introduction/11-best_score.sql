643359261055674a5d87a6d76f7b9c4f24edf1ff
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-12 18:49:58 +0000

    best score with spec

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- a script that lists all records with a score >= 10 
+-- in the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name (in this order)
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC; 
