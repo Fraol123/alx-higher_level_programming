7a0da4613008b7c60ef97dfc1593fb75e901bcf6
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-12 17:48:26 +0000

    add values

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- a script that inserts a new row in the table first_table in your MySQL server.
+-- New row:
+
+--    id = 89
+--    name = Holberton School
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School')
