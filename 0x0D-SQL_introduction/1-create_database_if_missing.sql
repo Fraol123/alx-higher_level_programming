6fda8dda239d175127c670e79f33531fe91d097b
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-12 17:11:38 +0000

    if not exist dont fail

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- a script that creates the database hbtn_0c_0
+-- If the database hbtn_0c_0 already exists it should not fail
+-- SELECT or SHOW statements are not allowed
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
