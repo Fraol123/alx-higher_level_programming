9921a21d51a84ad0c9ee26931778d31980d2cd76
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-13 18:05:29 +0000

    generes

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,14 @@
+-- lists all generes of the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (But the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genere name
+-- You can use only one SELECT statement
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title ="Dexter"
+ORDER BY tv_genres.name;
