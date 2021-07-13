30ca1b15fdab429da2cb7e6c8a957c6e11c4d7b0
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-07-13 15:29:41 +0000

    defualt constraint

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- a script that creates the table id_not_null on your MySQL server.
+-- d_not_null description:
+--  id INT with the default value 1
+--  name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
