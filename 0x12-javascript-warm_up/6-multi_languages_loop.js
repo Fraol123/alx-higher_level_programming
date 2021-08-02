357452ee034b169e67a905e4265c30dc83ffd17d
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-08-02 12:52:42 +0000

    eroor

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const arg in args) {
   console.log(args[arg]);
 }
