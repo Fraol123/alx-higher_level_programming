361582cd1dd024ab460c9900e21e173a43ca62fe
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-08-02 08:43:50 +0000

    consol used

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myFirstLine  = "C is fun";
+const mySecondLine = "Python is Cool";
+const myThirdLine  = "JavaScript is amazing";
+console.log(myFirstLine);
+console.log(mySecondLine);
+console.log(myThirdLine); 
