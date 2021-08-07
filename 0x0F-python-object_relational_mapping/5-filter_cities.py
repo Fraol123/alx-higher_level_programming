b6dcf4fb5cdf934a4630a6005802c3f8908ad002
root <fraol123>
2021-08-07 18:25:37 +0000

    made it print horizontally

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    print(", ".join(city[0] for city in rows))
     cursor.close()
     db.close()
