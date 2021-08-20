3721734019270c4b104d18ce8b9f3ea059f3cdbf
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-08-20 14:18:39 +0000

    type shown

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as r:
         html = r.read()
         print('Body response:')
-        print("\t- type: {}".format(html))
+        print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
